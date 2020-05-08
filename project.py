0ffedee0b502786e03473e88eb80a7c55267bc47
AlexB <alexandrebouillon@gmail.com>
2020-05-08 15:23:37 +0200

    Task 2

--- project.py
+++ project.py
@@ -5,12 +5,13 @@ import csv
 class ResultValues():
 
     def __init__(self):
-        
         # Do computations here
-        id3=ID3()
-        self.filename = str(input("nom du fichier a ouvrir"))
+        id3 = ID3()
+        self.filename = "train_bin.csv" #str(input("nom du fichier a ouvrir"))
         # Task 1
         self.arbre = id3.construit_arbre(self.get_datas())
+        # Task 2
+        self.precision_rate = self.test_precision()
         # Task 3
         self.faits_initiaux = None
         self.regles = None
@@ -20,7 +21,7 @@ class ResultValues():
     def get_results(self):
         return [self.arbre, self.faits_initiaux, self.regles, self.arbre_advance]
 
-    def get_datas(self) :
+    def get_datas(self, filename=None):
         """
         Extrait les donnees du fichier csv
 
@@ -28,23 +29,37 @@ class ResultValues():
                  et un dictionnaire associant attributs et valeurs
 
         """
-        #the datas as a list of dictionnaries
-        donnees=[]
-        #open the file with encoding utf8-sig because of strange characters
-        with open(self.filename , newline ='\n',encoding='utf-8-sig') as datas :
-            #retrieves datas as a reader over dictionnaries
-            dict_reader=csv.DictReader(datas,delimiter=',')
-            #converts each row from OrderedDict to Dictionnary
-            #Creates a list of datas
-            for dct in map(dict,dict_reader):
-                single_data=[]
+        if filename is None:
+            filename = self.filename
+        # the datas as a list of dictionnaries
+        donnees = []
+        # open the file with encoding utf8-sig because of strange characters
+        with open(filename, newline='\n', encoding='utf-8-sig') as datas:
+            # retrieves datas as a reader over dictionnaries
+            dict_reader = csv.DictReader(datas, delimiter=',')
+            # converts each row from OrderedDict to Dictionnary
+            # Creates a list of datas
+            for dct in map(dict, dict_reader):
+                single_data = []
                 single_data.append(dct.pop("target"))
                 single_data.append(dct)
                 donnees.append(single_data)
 
         return donnees
 
-
-
-
-
+    def test_precision(self):
+        donnees_test = self.get_datas("test_public_bin.csv")
+        total = len(donnees_test)
+        # Extraction of class value for each subject
+        results = [d[0] for d in donnees_test]
+        # Prediction of each subject's class using the model
+        results_test = [self.arbre.classifie(d[1]) for d in donnees_test]
+        # Precision calculation
+        precision = 0
+        if len(results) == len(results_test):
+            for i, j in zip(results, results_test):
+                if str(i) == j[-1]:
+                    precision = precision + 1
+            return (precision / total) * 100
+        else:
+            raise NameError("Error: number of results != number of predictions")
